9cfe2427b29b03174809d4eac12c3f3fda2e3297
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-07-10 11:55:13 -0400

    Create api namespaced User and Session controllers, add corresponding api routes

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,15 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,11 @@
+class Api::UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+    
+  end
+
+end
