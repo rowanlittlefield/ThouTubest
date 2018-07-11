7bbed3bdc1e60ccc98afccc8d8061cf73fe55105
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-07-11 12:11:30 -0400

    debug seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-
-u1 = User.new(username: 'Todd', email: '1@aol.com', image_url: 'dummy', password: 'passwurd')
-file1 = File.open('/Users/rowanlittlefield/Desktop/cassowary.jpeg')
-u1.photo.attach(io: file1, filename: 'cassowary.jpeg')
-u1.save!
+#
+# u1 = User.new(username: 'Todd', email: '1@aol.com', image_url: 'dummy', password: 'passwurd')
+# file1 = File.open('/Users/rowanlittlefield/Desktop/cassowary.jpeg')
+# u1.photo.attach(io: file1, filename: 'cassowary.jpeg')
+# u1.save!
