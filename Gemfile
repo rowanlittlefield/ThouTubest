550ffccfac036fb4afb8dbc46929b107dcdc345c
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-07-13 09:58:26 -0400

    Refactor seeds using Oscar's EzDownload gem

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'figaro'
 gem 'jquery-rails'
 gem 'aws-sdk-s3', '~>1'#, require: false
 gem 'mini_magick', '~> 4.8'
+gem 'ez_download', git: 'https://github.com/OscarAlvarez8830/EZDownload.git'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# User.destroy_all
+db_type = "prod"
+
 User.destroy_all
 
 u1 = User.new(username: 'Todd', email: '1@aol.com', image_url: 'dummy', password: 'passwurd')
-file1 = File.open('/Users/rowanlittlefield/Desktop/cassowary.jpeg')
-u1.photo.attach(io: file1, filename: 'cassowary.jpeg')
+file = EzDownload.open("https://s3.amazonaws.com/thoutubest-#{db_type}/cassowary.jpeg")
+u1.photo.attach(io: file, filename: 'cassowary.jpeg')
 u1.save!
 
 Video.destroy_all
@@ -20,31 +23,9 @@ Video.destroy_all
     video_url: 'dummy', thumbnail_url: 'dummy', views: 0,
     uploader_id: u1.id
    )
-  thumbnail_image_file1 = File.open('/Users/rowanlittlefield/Desktop/cassowary.jpeg')
+  thumbnail_image_file1 = EzDownload.open("https://s3.amazonaws.com/thoutubest-#{db_type}/cassowary.jpeg")
   v1.thumbnail_image.attach(io: thumbnail_image_file1, filename: 'cassowary.jpeg')
-  film_file1 = File.open('/Users/rowanlittlefield/Desktop/test_video.mov')
+  film_file1 = EzDownload.open("https://s3.amazonaws.com/thoutubest-#{db_type}/test_video.mov")
   v1.film.attach(io: film_file1, filename: 'test_video.mov')
   v1.save!
 end
-
-# v1 = Video.new(
-#   title: 'Test Video', description: 'Test Video description',
-#   video_url: 'dummy', thumbnail_url: 'dummy', views: 0,
-#   uploader_id: u1.id
-#  )
-# thumbnail_image_file1 = File.open('/Users/rowanlittlefield/Desktop/cassowary.jpeg')
-# v1.thumbnail_image.attach(io: thumbnail_image_file1, filename: 'cassowary.jpeg')
-# film_file1 = File.open('/Users/rowanlittlefield/Desktop/test_video.mov')
-# v1.film.attach(io: film_file1, filename: 'test_video.mov')
-# v1.save!
-#
-# v2 = Video.new(
-#   title: 'Test Video', description: 'Test Video description',
-#   video_url: 'dummy', thumbnail_url: 'dummy', views: 0,
-#   uploader_id: u1.id
-#  )
-# thumbnail_image_file2 = File.open('/Users/rowanlittlefield/Desktop/cassowary.jpeg')
-# v2.thumbnail_image.attach(io: thumbnail_image_file2, filename: 'cassowary.jpeg')
-# film_file2 = File.open('/Users/rowanlittlefield/Desktop/test_video.mov')
-# v2.film.attach(io: film_file2, filename: 'test_video.mov')
-# v2.save!
