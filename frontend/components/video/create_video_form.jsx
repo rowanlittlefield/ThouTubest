daf0d54dbadb7eb2981c8824e4d5652eb5685758
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-07-14 15:33:04 -0400

    Continue styling on CreateVideoForm

--- frontend/components/video/create_video_form.jsx
+++ frontend/components/video/create_video_form.jsx
@@ -109,17 +109,35 @@ class CreateVideoForm extends React.Component {
     }
   }
 
+  thumbnailPreview() {
+    if (this.state.thumbnailUrl) {
+      return (<img width='95px' height="95px" src={this.state.thumbnailUrl} />);
+    } else {
+      // return null;
+      return (<div className="create-video-thumbnail-preview" width='60px' height="60px">
+      <span>Thumbnail</span> <span>Preview</span>
+      </div>);
+    }
+  }
+
+  filmPreview() {
+    if (this.state.filmUrl) {
+      return (<video width='340px' height="300px" src={this.state.filmUrl} />);
+    } else {
+      return (<div className="create-video-film-preview" width='200px' height="200px">
+      <span>Video</span> <span>Preview</span>
+      </div>);
+    }
+  }
+
   render() {
     console.log(this.state);
-    const thumbnailPreview = this.state.thumbnailUrl ? <img width='60px' height="60px" src={this.state.thumbnailUrl} /> : null;
-    const filmPreview = this.state.filmUrl ? <video width="200px" height="120px" src={this.state.filmUrl} /> : null;
 
     return (
       <div>
         <form className="create-video-form" onClick={this.clearErrors.bind(this)} onSubmit={this.handleSubmit}>
 
           <br/>
-          {this.renderErrors()}
           <div className="create-video-right-col">
             <div className="create-video-title-box">
               <label>Title
@@ -137,11 +155,13 @@ class CreateVideoForm extends React.Component {
             </div>
 
             <div className="create-video-thumbnail-upload">
-              <label>Thumbnail Upload
+              <label className="create-video-thumbnail-label">Thumbnail Upload
                 <input type="file"
-                  onChange={this.handleFile('thumbnail')}/>
+                  onChange={this.handleFile('thumbnail')}
+                  className="create-video-thumbnail-input"/>
               </label>
-              {thumbnailPreview}
+              <img className="create-video-thumbnail-icon" src={window.uploadFileIcon} width="60px" height="60px"/>
+              {this.thumbnailPreview()}
             </div>
             <br/>
 
@@ -149,13 +169,18 @@ class CreateVideoForm extends React.Component {
 
           <div className="create-video-left-col">
             <div className="create-video-film-upload">
-              <label>Video Upload
-                <input type="file"
-                  onChange={this.handleFile('film')}/>
+              <label className="create-video-film-upload-label">Video Upload
+                <div className="create-video-film-input-adjust">
+                  <input type="file"
+                    className="create-video-film-input"
+                    onChange={this.handleFile('film')}/>
+                </div>
               </label>
-              {filmPreview}
+              <img className="create-video-film-icon" src={window.uploadFileIcon} width="60px" height="60px"/>
+              {this.filmPreview()}
+              <input className="create-video-submit" type="submit" value="next"/>
+              {this.renderErrors()}
             </div>
-            <input type="submit" value={this.props.formType}/>
           </div>
 
 
