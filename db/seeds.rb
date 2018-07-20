542a4ac59d2de0639c0b10d4c0e5fa09f3ea9465
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-07-20 11:26:42 -0400

    debug heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# User.destroy_all
+
 require 'streamio-ffmpeg'
 User.destroy_all
 
