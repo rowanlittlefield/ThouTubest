cc6facee74125afddf21180c322c096795deb611
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-07-26 17:36:22 -0400

    Remove users index controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :index, :show]
+    resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy, :show]
     resources :videos, only: [:index, :show, :create, :update, :destroy]
     resources :comments, only: [:index, :create, :update, :destroy]
