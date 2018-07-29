55c1131ec6b0d795900bde804ec74d3ae3db4082
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-07-29 14:41:49 -0400

    Restore seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,14 +64,14 @@ user_options_list = [
   {username: 'Cassowary', email: '1@aol.com', password: 'passwurd',
   img_url: "http://s3.amazonaws.com/thoutubest-dev/cassowary.jpeg"},
 
-  # {username: 'Penguin', email: '2@aol.com', password: 'passwurd',
-  # img_url: "https://s3.amazonaws.com/thoutubest-dev/users/penguin.jpg"},
-  #
-  # {username: 'Cardinal', email: '3@aol.com', password: 'passwurd',
-  # img_url: "https://s3.amazonaws.com/thoutubest-dev/users/cardinal.jpg"},
-  #
-  # {username: 'Pelican', email: '4@aol.com', password: 'passwurd',
-  # img_url: "https://s3.amazonaws.com/thoutubest-dev/users/pelican.jpg"},
+  {username: 'Penguin', email: '2@aol.com', password: 'passwurd',
+  img_url: "https://s3.amazonaws.com/thoutubest-dev/users/penguin.jpg"},
+
+  {username: 'Cardinal', email: '3@aol.com', password: 'passwurd',
+  img_url: "https://s3.amazonaws.com/thoutubest-dev/users/cardinal.jpg"},
+
+  {username: 'Pelican', email: '4@aol.com', password: 'passwurd',
+  img_url: "https://s3.amazonaws.com/thoutubest-dev/users/pelican.jpg"},
 
   {username: 'Birdman', email: '5@aol.com', password: 'passwurd',
   img_url: "https://s3.amazonaws.com/thoutubest-dev/users/Birdman.jpg"}
@@ -79,10 +79,10 @@ user_options_list = [
 ]
 
 video_options_list = [
-  # {title: 'Test Video', description: 'Test video description',
-  # video_url: 'dummy', thumbnail_url: 'dummy',
-  # image_url: "http://s3.amazonaws.com/thoutubest-dev/cassowary.jpeg",
-  # film_url: "http://s3.amazonaws.com/thoutubest-dev/test_video.mov"},
+  {title: 'Test Video', description: 'Test video description',
+  video_url: 'dummy', thumbnail_url: 'dummy',
+  image_url: "http://s3.amazonaws.com/thoutubest-dev/cassowary.jpeg",
+  film_url: "http://s3.amazonaws.com/thoutubest-dev/test_video.mov"},
 
   {title: 'Hooded Oriole', description: 'A hodded oriole',
   video_url: 'dummy', thumbnail_url: 'dummy',
@@ -90,23 +90,23 @@ video_options_list = [
   film_url: "http://s3.amazonaws.com/thoutubest-dev/hooded_oriole/HOOR_20090731PM011200_os_ManyBirds-video-of-hooded-oriole.MP4"
   },
 
-  # {title: 'Buff Breasted Sandpiper', description: 'A really cool bird',
-  # video_url: 'dummy', thumbnail_url: 'dummy',
-  # image_url: "http://s3.amazonaws.com/thoutubest-dev/buff_breasted_sandpiper/Buff-breasted_Sandpiper_c27-6-108_l_1.jpg",
-  # film_url: "http://s3.amazonaws.com/thoutubest-dev/buff_breasted_sandpiper/BBSA_20090506AM080900_os_ManyBirds-video-of-buff-breasted-sandpiper.MP4"
-  # },
-  #
-  # {title: 'Spotted Owl', description: 'A really really cool bird',
-  # video_url: 'dummy', thumbnail_url: 'dummy',
-  # image_url: "http://s3.amazonaws.com/thoutubest-dev/spotted_owl/spotted_owl_07.jpg",
-  # film_url: "http://s3.amazonaws.com/thoutubest-dev/spotted_owl/SPOW_20090729AM101000_os_ManyBirds-video-of-spotted-owl.MP4"
-  # },
-  #
-  # {title: 'Shoebill Stork', description: 'A beautiful bird',
-  # video_url: 'dummy', thumbnail_url: 'dummy',
-  # image_url: "http://s3.amazonaws.com/thoutubest-dev/shoebill/shoebill_meme.jpg",
-  # film_url: "http://s3.amazonaws.com/thoutubest-dev/shoebill/Shoebill.mp4"
-  # },
+  {title: 'Buff Breasted Sandpiper', description: 'A really cool bird',
+  video_url: 'dummy', thumbnail_url: 'dummy',
+  image_url: "http://s3.amazonaws.com/thoutubest-dev/buff_breasted_sandpiper/Buff-breasted_Sandpiper_c27-6-108_l_1.jpg",
+  film_url: "http://s3.amazonaws.com/thoutubest-dev/buff_breasted_sandpiper/BBSA_20090506AM080900_os_ManyBirds-video-of-buff-breasted-sandpiper.MP4"
+  },
+
+  {title: 'Spotted Owl', description: 'A really really cool bird',
+  video_url: 'dummy', thumbnail_url: 'dummy',
+  image_url: "http://s3.amazonaws.com/thoutubest-dev/spotted_owl/spotted_owl_07.jpg",
+  film_url: "http://s3.amazonaws.com/thoutubest-dev/spotted_owl/SPOW_20090729AM101000_os_ManyBirds-video-of-spotted-owl.MP4"
+  },
+
+  {title: 'Shoebill Stork', description: 'A beautiful bird',
+  video_url: 'dummy', thumbnail_url: 'dummy',
+  image_url: "http://s3.amazonaws.com/thoutubest-dev/shoebill/shoebill_meme.jpg",
+  film_url: "http://s3.amazonaws.com/thoutubest-dev/shoebill/Shoebill.mp4"
+  },
 
   {title: 'Magnificent Riflebird', description: 'A really beautiful bird',
   video_url: 'dummy', thumbnail_url: 'dummy',
@@ -114,29 +114,29 @@ video_options_list = [
   film_url: "http://s3.amazonaws.com/thoutubest-dev/magnificent_rifle_bird/magnificent_riflebird.mp4"
   },
 
-  # {title: 'Red winged black bird', description: 'A beautiful bird',
-  # video_url: 'dummy', thumbnail_url: 'dummy',
-  # image_url: "http://s3.amazonaws.com/thoutubest-dev/redwing_blackbird/redwing_blackbird.jpeg",
-  # film_url: "http://s3.amazonaws.com/thoutubest-dev/redwing_blackbird/redwing_blackbird.mp4"
-  # },
-  #
-  # {title: 'Arizona woodpecker', description: 'A nice looking bird',
-  # video_url: 'dummy', thumbnail_url: 'dummy',
-  # image_url: "http://s3.amazonaws.com/thoutubest-dev/arizona_woodpecker/Arizona_woodpecker.JPG",
-  # film_url: "http://s3.amazonaws.com/thoutubest-dev/arizona_woodpecker/ARWO_20090731PM012000_os_ManyBirds-video-of-arizona-woodpecker.MP4"
-  # },
-  #
-  # {title: 'Violet-crowned hummingbird', description: 'A nice looking bird',
-  # video_url: 'dummy', thumbnail_url: 'dummy',
-  # image_url: "http://s3.amazonaws.com/thoutubest-dev/purple-crowned-hummingbird/Purple-crowned_hummingbird.jpg",
-  # film_url: "http://s3.amazonaws.com/thoutubest-dev/purple-crowned-hummingbird/VCHU_20090730PM033200_os_ManyBirds-video-of-violet-crowned-hummingbird.MP4"
-  # },
-  #
-  # {title: 'Rooster', description: 'a cooooooooool bird',
-  # video_url: 'dummy', thumbnail_url: 'dummy',
-  # image_url: "http://s3.amazonaws.com/thoutubest-dev/rooster/rooster.jpg",
-  # film_url: "http://s3.amazonaws.com/thoutubest-dev/rooster/Pexels+Videos+4644.mp4"
-  # }
+  {title: 'Red winged black bird', description: 'A beautiful bird',
+  video_url: 'dummy', thumbnail_url: 'dummy',
+  image_url: "http://s3.amazonaws.com/thoutubest-dev/redwing_blackbird/redwing_blackbird.jpeg",
+  film_url: "http://s3.amazonaws.com/thoutubest-dev/redwing_blackbird/redwing_blackbird.mp4"
+  },
+
+  {title: 'Arizona woodpecker', description: 'A nice looking bird',
+  video_url: 'dummy', thumbnail_url: 'dummy',
+  image_url: "http://s3.amazonaws.com/thoutubest-dev/arizona_woodpecker/Arizona_woodpecker.JPG",
+  film_url: "http://s3.amazonaws.com/thoutubest-dev/arizona_woodpecker/ARWO_20090731PM012000_os_ManyBirds-video-of-arizona-woodpecker.MP4"
+  },
+
+  {title: 'Violet-crowned hummingbird', description: 'A nice looking bird',
+  video_url: 'dummy', thumbnail_url: 'dummy',
+  image_url: "http://s3.amazonaws.com/thoutubest-dev/purple-crowned-hummingbird/Purple-crowned_hummingbird.jpg",
+  film_url: "http://s3.amazonaws.com/thoutubest-dev/purple-crowned-hummingbird/VCHU_20090730PM033200_os_ManyBirds-video-of-violet-crowned-hummingbird.MP4"
+  },
+
+  {title: 'Rooster', description: 'a cooooooooool bird',
+  video_url: 'dummy', thumbnail_url: 'dummy',
+  image_url: "http://s3.amazonaws.com/thoutubest-dev/rooster/rooster.jpg",
+  film_url: "http://s3.amazonaws.com/thoutubest-dev/rooster/Pexels+Videos+4644.mp4"
+  }
 
 ]
 
