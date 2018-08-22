ccf84cacf0279c83dcbf7a1a6387964023344688
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-08-22 13:18:24 -0400

    Refactor LikesReducer

--- frontend/reducers/entities/likes_reducer.js
+++ frontend/reducers/entities/likes_reducer.js
@@ -5,16 +5,14 @@ import { RECEIVE_VIDEO } from '../../actions/video_actions'
 import { RECEIVE_LIKE, REMOVE_LIKE } from '../../actions/like_actions';
 
 const likesReducer = (state = {}, action) => {
-
   Object.freeze(state);
 
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
-      return merge({}, state, action.likes)
-    case RECEIVE_LIKE:
-      return merge({}, state, {[action.like.id]: action.like})
     case RECEIVE_VIDEO:
-      return merge({}, state, (action.likes ? action.likes : {}));
+      return merge({}, state, action.likes);
+    case RECEIVE_LIKE:
+      return merge({}, state, {[action.like.id]: action.like});
     case REMOVE_LIKE:
       const newState = merge({}, state);
       delete newState[action.like.id];
