b98d95da534a6400a7be1852be44dc1735b1e220
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-08-23 12:52:22 -0400

    Remove commented out code

--- frontend/reducers/selectors/comment_selectors.js
+++ frontend/reducers/selectors/comment_selectors.js
@@ -13,17 +13,3 @@ export const filterComments = (
 
   return filteredCommentIds;
 };
-
-
-// const currentVideoId = ownProps.match.params.videoId;
-// const parentCommentId = ownProps.parentCommentId;
-// const commentIds = (state.entities.videos[currentVideoId] ?
-//   state.entities.videos[currentVideoId].comment_ids : []);
-//
-// for(let i = 0; i < commentIds.length; i++) {
-//   const comment = state.entities.comments[commentIds[i]];
-//   if(comment && `${comment.video_id}` === currentVideoId &&
-//     comment.parent_comment_id === parentCommentId) {
-//     filteredCommentIds.push(commentIds[i]);
-//   }
-// }
