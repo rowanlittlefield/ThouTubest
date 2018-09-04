cab7d09f1d704f3babb27b6d9ec812715b3c0023
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-09-03 20:40:38 -0400

    Refactor navbar into functional presentational component

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -4,9 +4,9 @@ import SearchBarContainer from './search_bar';
 import MainHambugerDropDownContainer from './main_hamburger_drop_down';
 import UserNavbarMenu from './user_navbar_menu';
 
-class Navbar extends React.Component {
+const Navbar = ({ currentUser }) => {
 
-  sessionLinks() {
+  function sessionLinks() {
     return (
       <span>
         <Link className="navbar-signup-link" to="/login">Sign In</Link>
@@ -14,40 +14,36 @@ class Navbar extends React.Component {
     );
   }
 
-  render() {
-    const currentUser = this.props.currentUser;
+  return (
+    <nav className="navbar">
+      <div className="navbar-left-nav">
+        <ul className="main-hamburger-button-tl-ul">
+          <li>
+            <img className="main-hamburger-button" width="24px" height="24px" src={window.hamburgerButton}
+              onClick={() => {
+                const mainHamDropdown = document.getElementById("main-hamburger-dropdown");
+                mainHamDropdown.classList.toggle('hidden');
+              }} />
+          </li>
+          <li>
+            <MainHambugerDropDownContainer />
+          </li>
 
-    return (
-      <nav className="navbar">
-        <div className="navbar-left-nav">
-          <ul className="main-hamburger-button-tl-ul">
-            <li>
-              <img className="main-hamburger-button" width="24px" height="24px" src={window.hamburgerButton}
-                onClick={() => {
-                  const mainHamDropdown = document.getElementById("main-hamburger-dropdown");
-                  mainHamDropdown.classList.toggle('hidden');
-                }} />
-            </li>
-            <li>
-              <MainHambugerDropDownContainer />
-            </li>
+        </ul>
+        <img className="navbar-left-nav-logo"
+           src={window.logoWithText} width="135px" height="45px" />
+      </div>
 
-          </ul>
-          <img className="navbar-left-nav-logo"
-             src={window.logoWithText} width="135px" height="45px" />
-        </div>
+      <SearchBarContainer />
 
-        <SearchBarContainer />
-
-        <div className="navbar-right-nav">
-          <Link to={currentUser ? "/videos/new" : "/login"}>
-            <img id="add-video-icon" width="40" heigh="40" src={window.addVideoIcon}/>
-          </Link>
-          {currentUser ? <UserNavbarMenu /> : this.sessionLinks()}
-        </div>
-      </nav>
-    );
-  }
+      <div className="navbar-right-nav">
+        <Link to={currentUser ? "/videos/new" : "/login"}>
+          <img id="add-video-icon" width="40" heigh="40" src={window.addVideoIcon}/>
+        </Link>
+        {currentUser ? <UserNavbarMenu /> : sessionLinks()}
+      </div>
+    </nav>
+  );
 }
 
 
